13ee9a3bb516e24433bb58cba9e52453f35715e7
Timmy <timmy85618@gmail.com>
2023-08-28 10:53:38 +0800

    add .env file

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+from datetime import timedelta
+from dotenv import load_dotenv
+
+# This will load the environment variables from .env file
+load_dotenv()
+
+
+class Config:
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    PORT = int(os.environ.get('PORT', 5001))
+    DEBUG = bool(os.environ.get('DEBUG', True))
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'mysecretkey')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'SQLALCHEMY_DATABASE_URI', 'mysql+pymysql://admin:admin@localhost:3306/db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'dst-super-secret')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
+    DST_MSSQL = os.environ.get(
+        'DST_MSSQL', 'Driver={FreeTDS};Server=ip;port=4876;UID=sa;PWD=password;Database=db')
