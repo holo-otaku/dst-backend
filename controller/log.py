3b75b7dd25160beecaec02733408185caaa22dd4
Timmy <timmy85618@gmail.com>
2023-09-30 23:02:18 +0800

    log search improve

--- controller/log.py
+++ controller/log.py
@@ -1,5 +1,6 @@
 from flask import current_app, jsonify, make_response, request
 from models.log import ActivityLog
+from models.user import User
 from models.shared import db
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -41,17 +42,20 @@ def read_multi():
     try:
         # Get the limit and offset values from the request
         limit = int(request.args.get('limit', 10))
-        offset = int(request.args.get('page', 1))-1
+        offset = int(request.args.get('page', 1)) - 1
 
-        # Query the ActivityLog table with limit and offset
-        logs = ActivityLog.query.offset(offset*limit).limit(limit).all()
+        # Query the ActivityLog table with limit, offset, order_by and join with User
+        logs = db.session.query(ActivityLog, User.username).join(
+            User, ActivityLog.user_id == User.id
+        ).order_by(ActivityLog.created_at.desc()).offset(offset*limit).limit(limit).all()
 
         response_data = []
-        for log in logs:
+        for log, username in logs:
             response_data.append({
                 'id': log.id,
                 'url': log.url,
                 'userId': log.user_id,
+                'userName': username,
                 'payload': log.payload,
                 'createdAt': log.created_at.strftime('%Y-%m-%d %H:%M:%S')
             })
