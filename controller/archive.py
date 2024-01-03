8e6cced2605c4c10d831f4a10e142e92f457aa42
Timmy0618 <timmy85618@gmail.com>
2024-01-03 11:07:40 +0800

    change archive to two api

--- controller/archive.py
+++ controller/archive.py
@@ -5,17 +5,45 @@ from sqlalchemy.exc import SQLAlchemyError
 from flask_jwt_extended import get_jwt_identity
 
 
-def update(item_id):
+def create(data):
+    try:
+        item_id = data.get('itemId')
+        user_id = get_jwt_identity()
+
+        # 检查是否已经存在相同的 Archive 记录
+        existing_archive = Archive.query.filter_by(
+            item_id=item_id, archived_by=user_id).first()
+
+        if existing_archive:
+            return make_response(jsonify({"code": 200, "msg": "Archive already exists"}), 200)
+
+        new_archive = Archive(item_id=item_id, archived_by=user_id)
+        db.session.add(new_archive)
+        db.session.commit()
+
+        return make_response(jsonify({"code": 200, "msg": "Success"}), 200)
+
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        return make_response(jsonify({"code": 500, "msg": str(e)}), 500)
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return make_response(jsonify({"code": 500, "msg": str(e)}), 500)
+
+    finally:
+        db.session.close()
+
+
+def delete(item_id):
     try:
         existing_archive = Archive.query.filter_by(item_id=item_id).first()
 
-        if existing_archive:
-            db.session.delete(existing_archive)
-        else:
-            user_id = get_jwt_identity()
-            new_archive = Archive(
-                item_id=item_id, archived_by=user_id)
-            db.session.add(new_archive)
+        if not existing_archive:
+            return make_response(jsonify({"code": 400, "msg": "Item not exist"}), 400)
+
+        db.session.delete(existing_archive)
 
         db.session.commit()
 
--- routers/archive.py
+++ routers/archive.py
@@ -1,11 +1,20 @@
-from flask import Blueprint
-from controller.archive import update
+from flask import Blueprint, request
+from controller.archive import create, delete
 from controller.access import check_permission
 
 archive = Blueprint("archive", __name__)
 
-@archive.route('/<int:item_id>', methods=['PATCH'])
-@check_permission('archive.update')
-def edit_archive(item_id):
 
-    return update(item_id)
+@archive.route("", methods=['POST'])
+@check_permission('archive.create')
+def create_archive():
+    data = request.get_json()
+
+    return create(data)
+
+
+@archive.route('/<int:item_id>', methods=['DELETE'])
+@check_permission('archive.delete')
+def delete_archive(item_id):
+
+    return delete(item_id)
