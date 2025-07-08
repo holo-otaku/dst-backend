6206d8ef1a68623315c913baf99d320d5e16fa16
Timmy <timmy85618@gmail.com>
2025-07-08 15:20:35 +0800

    Add export functionality for products to Excel and update dependencies
    - Implemented a new endpoint for exporting filtered products in a series to an Excel file.
    - Added xlsxwriter as a dependency in Pipfile and Pipfile.lock.
    - Updated requirements.txt to include necessary packages.
    - Refactored product routes to include the new export functionality.
    - Enhanced Swagger documentation for the export endpoint.

--- routers/product.py
+++ routers/product.py
@@ -1,12 +1,19 @@
 from flask import Blueprint, request
-from controller.product import create, read, delete, update_multi, read_multi
+from controller.product import (
+    create,
+    read,
+    delete,
+    update_multi,
+    read_multi,
+    export_excel,
+)
 from controller.access import check_permission
 
 products = Blueprint("products", __name__)
 
 
 @products.route("", methods=["POST"])
-@check_permission('product.create')
+@check_permission("product.create")
 def create_product():
     data = request.get_json()
 
@@ -14,15 +21,23 @@ def create_product():
 
 
 @products.route("/search", methods=["POST"])
-@check_permission('product.read')
+@check_permission("product.read")
 def read_multi_product():
     data = request.get_json()
 
     return read_multi(data)
 
 
+@products.route("/export", methods=["POST"])
+@check_permission("product.read")
+def export_product():
+    data = request.get_json()
+
+    return export_excel(data)
+
+
 @products.route("/edit", methods=["PATCH"])
-@check_permission('product.edit')
+@check_permission("product.edit")
 def edit_product():
     data = request.get_json()
 
@@ -30,7 +45,7 @@ def edit_product():
 
 
 @products.route("/delete", methods=["DELETE"])
-@check_permission('product.delete')
+@check_permission("product.delete")
 def delete_product():
     data = request.get_json()
 
@@ -38,7 +53,7 @@ def delete_product():
 
 
 @products.route("/<int:product_id>", methods=["GET"])
-@check_permission('product.read')
+@check_permission("product.read")
 def read_product(product_id):
 
     return read(product_id)
